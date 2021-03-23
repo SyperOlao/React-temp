111d8d115988b975b63b11831ea3b2b892d97f1a
SyperOlao <olay1234538@gmail.com>
2021-03-23 13:47:33 +0300

    test commit

--- src/Components/Content/Profile Page/Profile.jsx
+++ src/Components/Content/Profile Page/Profile.jsx
@@ -14,7 +14,7 @@ export const Profile = (props) => {
             <div className={classes.info}>
                 <UserInfo state={props.state.info[0]} online={true}/>
                 <WritePost/>
-                <Post text="post" amountLikes={43} amountDisLikes={21}/>
+                <Post text="post" amountLikes={42} amountDisLikes={21}/>
             </div>
 
         </div>
